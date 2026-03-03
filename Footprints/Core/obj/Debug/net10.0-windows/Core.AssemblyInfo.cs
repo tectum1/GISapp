1af2bdac16f8ae4f8bda31c1dc44176a940f6e07
unknown <dylaneckstein@hotmail.com>
2026-03-03 14:16:55 -0600

    Added ReadMe

--- Footprints/Core/obj/Debug/net10.0-windows/Core.AssemblyInfo.cs
+++ Footprints/Core/obj/Debug/net10.0-windows/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc1e1e8ad50cbc84802cb0333bd7210e011619a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b61e81065e50cd19e62c067e986d63f3218b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Footprints/WinForms/obj/Debug/net10.0-windows/WinForms.AssemblyInfo.cs
+++ Footprints/WinForms/obj/Debug/net10.0-windows/WinForms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc1e1e8ad50cbc84802cb0333bd7210e011619a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b61e81065e50cd19e62c067e986d63f3218b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
